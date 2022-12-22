3fc1b0f3cfcd7e1027c52bed564267e243a5e3f9
Joseph M <jmedina1985@hotmail.com>
2022-12-21 22:36:07 -0800

    TS object

--- myObjects.ts
+++ myObjects.ts
@@ -1,17 +1,17 @@
-const User = {
-  name: "joey",
-  email: "j@jm.com",
-  isActive: true
-}
+// const User = {
+//   name: "joey",
+//   email: "j@jm.com",
+//   isActive: true
+// }
 
-function createUser({name: string, isPaid: boolean}){}
+// function createUser({name: string, isPaid: boolean}){}
 
-let newUser = {name: "joey", isPaid: false, email: "j@j.com"}
+// let newUser = {name: "joey", isPaid: false, email: "j@j.com"}
 
-createUser(newUser)
+// createUser(newUser)
 
-function createCourse():{name: string, price: number}{
-  return {name: "reactJS", price: 399}
-}
+// function createCourse():{name: string, price: number}{
+//   return {name: "reactJS", price: 399}
+// }
 
 export {}

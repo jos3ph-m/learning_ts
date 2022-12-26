0fdf00540e40ad7e69a38baab9f3389e549ead83
Joseph M <jmedina1985@hotmail.com>
2022-12-25 22:27:04 -0800

    some ts arrays

--- myArray.ts
+++ myArray.ts
@@ -1,6 +1,6 @@
 const superHeroes: string[] = []
 // const heroPower: number[] = []
-const heroPower: number[] = []
+const heroPower: Array<number> = []
 
 superHeroes.push("spiderman")
 heroPower.push(3)

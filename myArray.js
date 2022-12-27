d1bcf38d23c8a9aff10791d651b79497b3fbaae2
Joseph M <jmedina1985@hotmail.com>
2022-12-26 21:04:50 -0800

    union types

---
+++ myArray.js
@@ -0,0 +1,11 @@
+var superHeroes = [];
+// const heroPower: number[] = []
+var heroPower = [];
+var allUsers = [];
+var MLModels = [
+    [255, 255, 255],
+    [124, 124, 124]
+];
+superHeroes.push("spiderman");
+heroPower.push(3);
+allUsers.push({ name: "", isActive: true });

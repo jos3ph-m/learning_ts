0b9d7b6fd3605e7afaa80ef469c74f898f17593c
Joseph M <jmedina1985@hotmail.com>
2022-12-27 20:49:32 -0800

    Tuples

--- tuples.ts
+++ tuples.ts
@@ -1,6 +1,8 @@
 // const user: (string | number)[] = [1, 'hc']
 let tUser: [string, number, boolean]
 
+// tuple ensures you use the above types in that order
+
 tUser = ["hc", 131, true]
 
 export {}

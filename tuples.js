9eeebbfc77dbfacb3ef9f7d44eb4c09c8e51a0f8
Joseph M <jmedina1985@hotmail.com>
2022-12-28 21:48:00 -0800

    Tuples

---
+++ tuples.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+// const user: (string | number)[] = [1, 'hc']
+var tUser;
+// tuple ensures you use the above types in that order
+tUser = ["hc", 131, true];
+var rgb = [255, 123, 112];
+var newUser = [112, "example@g.com"];
+newUser[1] = "hc.com";

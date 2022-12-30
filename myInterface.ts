620f1da73084b17b8b73dfaf11ebdff8c2b853ba
Joseph M <jmedina1985@hotmail.com>
2022-12-29 22:08:46 -0800

    Interfaces

--- myInterface.ts
+++ myInterface.ts
@@ -4,7 +4,8 @@ interface User {
   userId: number,
   googleId?: string,
   // startTrial: () => string
-  startTrial(): string
+  startTrial(): string,
+  getCoupon(couponname: string): number,
 }
 
 const joey: User = {dbId: 22, email: "j@j.com", userId: 2211, startTrial: () => {return "trial started"}}

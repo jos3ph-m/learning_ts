f844c30f265cd41e8e2923e9f99f389f21fe5e35
Joseph M <jmedina1985@hotmail.com>
2022-12-30 23:29:02 -0800

    Interfaces

---
+++ myinterface.js
@@ -0,0 +1,4 @@
+var joey = { dbId: 22, email: "j@j.com", userId: 2211, startTrial: function () { return "trial started"; }, getCoupon: function (name, off) {
+        return 10;
+    } };
+joey.email = "j@jm.com";

3708a14df75e3bb0e3b86cc003b60006e1577881
Joseph M <jmedina1985@hotmail.com>
2022-12-30 23:35:37 -0800

    Interfaces

--- myinterface.js
+++ myinterface.js
@@ -1,4 +1,6 @@
-var joey = { dbId: 22, email: "j@j.com", userId: 2211, startTrial: function () { return "trial started"; }, getCoupon: function (name, off) {
+var joey = { dbId: 22, email: "j@j.com", userId: 2211, githubToken: "github", role: "admin",
+    startTrial: function () { return "trial started"; },
+    getCoupon: function (name, off) {
         return 10;
     } };
 joey.email = "j@jm.com";

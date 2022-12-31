58655a7807680ec266d08c4b0e47e66533ff1c2f
Joseph M <jmedina1985@hotmail.com>
2022-12-30 23:34:44 -0800

    Interfaces

--- myInterface.ts
+++ myInterface.ts
@@ -16,7 +16,7 @@ interface Admin extends User {
   role: "admin" | "ta" | "learner"
 }
 
-const joey: Admin = {dbId: 22, email: "j@j.com", userId: 2211, githubToken: "github",
+const joey: Admin = {dbId: 22, email: "j@j.com", userId: 2211, githubToken: "github", role: "admin",
 startTrial: () => {return "trial started"}, 
 getCoupon: (name: "joey10", off: 10) => {
   return 10
